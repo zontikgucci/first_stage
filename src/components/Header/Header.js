f3b1d73914e89994d084f0a73adfa6b774641a2b
Vladislav <endlessloop@bk.ru>
2024-06-04 01:44:06 +0100

    adding block personal

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,26 @@
+import './header.scss'
+
 import { Menu } from '../component/Menu/Menu'
 import { Phone } from '../component/Phone/Phone'
-import './header.scss'
-import point from '../../assets/icons/point.svg'
 import { Geo } from '../component/Geo/Geo'
+import { Logo } from '../component/Logo/Logo'
+import { Catalog } from '../component/Catalog/Catalog'
+import { Personal } from '../component/Personal/Personal'
+
 
 export const Header = () => {
   return (
     <header className='header'>
       <div className="container">
-        <div className="header__body">
+        <div className="header__top">
           <Geo />
           <Menu />
           <Phone />
+        </div>
+        <div className="header__under">
+          <Logo />
+          <Catalog/>
+          <Personal/>
         </div>
       </div>
     </header>
