74662d1c1d4d0f68b72850511d1f5d70e5a7cbdf
Vlad Gubar <endlessloop@bk.ru>
2024-06-08 01:02:18 +0300

    adding component Head

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,13 @@
+import { Head } from '../component/Head/Head'
 import './main.scss'
-
 
 export const Main = () => {
   return (
-    <main className='main'></main>
+    <main className='main'>
+      <Head head={'Спальня'}/>
+      <section className='content'></section>
+      <section className='compilation'></section>
+      <section className='variation'></section>
+    </main>
   )
 }
