5bea9521be63c8535dc7ec9de37d1bdd1cdf9083
Vlad Gubar <endlessloop@bk.ru>
2024-06-08 22:40:15 +0300

    Renamed Main -> Contents

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,13 @@
 import { Footer } from "../components/Footer/Footer"
 import { Header } from "../components/Header/Header"
-import { Main } from "../components/Main/Main"
+import { Contents } from "../components/Contents/Contents"
 
 
 export const Home = () => {
   return (
     <>
       <Header/>
-      <Main/>
+      <Contents/>
       <Footer/>
     </>
   )
