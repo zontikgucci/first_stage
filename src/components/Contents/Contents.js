01b0b3c82e3fe014ee6bdab9dfb1aba7e2955434
Vlad Gubar <endlessloop@bk.ru>
2024-06-09 04:37:13 +0300

    adding component Complilation

--- src/components/Contents/Contents.js
+++ src/components/Contents/Contents.js
@@ -1,14 +1,18 @@
 import { Head } from '../component/Head/Head'
 import { Content } from '../component/Content/Content'
+import { Compilation } from '../component/Compilation/Compilation'
+
+import { products } from '../data/products'
+
 import './contents.scss'
+
 
 export const Contents = () => {
   return (
     <main className='contents'>
       <Head head={'Спальня'}/>
-      <Content/>
-      <section className='content'></section>
-      <section className='compilation'></section>
+      <Content />
+      <Compilation dataProducts={products}/>
       <section className='variation'></section>
     </main>
   )
