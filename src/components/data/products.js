a0279cb123082affece38573f70888998dd17f02
Vlad Gubar <endlessloop@bk.ru>
2024-06-09 14:40:45 +0300

    create Object date products

---
+++ src/components/data/products.js
@@ -0,0 +1,100 @@
+export const products = [
+  {
+    id: 1,
+    title: 'Кровать «Триумф» 160',
+    price: {
+      priceString : '8 000 ₽',
+      priceNumber: 8000
+    },
+    checked: false,
+    absent: false,
+    rating: {
+      rate: 3.9,
+      count: 120
+    }
+  },
+  {
+    id: 2,
+    title: 'Матрас средний',
+    price: {
+      priceString : '10 000 ₽',
+      priceNumber: 10000
+    },
+    checked: false,
+    absent: false,
+    rating: {
+      rate: 4.2,
+      count: 200
+    }
+  },
+  {
+    id: 3,
+    title: 'Тумба прикроватная',
+    price: {
+      priceString : '2 000 ₽',
+      priceNumber: 2000
+    },
+    checked: false,
+    absent: true,
+    rating: {
+      rate: 5,
+      count: 77
+    }
+  },
+  {
+    id: 4,
+    title: 'Шкаф 4-дверный',
+    price: {
+      priceString : '10 000 ₽',
+      priceNumber: 10000
+    },
+    checked: false,
+    absent: false,
+    rating: {
+      rate: 3.2,
+      count: 148
+    }
+  },
+  {
+    id: 5,
+    title: 'Комод',
+    price: {
+      priceString : '2 000 ₽',
+      priceNumber: 2000
+    },
+    checked: false,
+    absent: false,
+    rating: {
+      rate: 4.5,
+      count: 234
+    }
+  },
+  {
+    id: 6,
+    title: 'Зеркало',
+    price: {
+      priceString : '2 000 ₽',
+      priceNumber: 2000
+    },
+    checked: false,
+    absent: false,
+    rating: {
+      rate: 4.9,
+      count: 209
+    }
+  },
+  {
+    id: 7,
+    title: 'Пуф',
+    price: {
+      priceString : '2 000 ₽',
+      priceNumber: 2000
+    },
+    checked: false,
+    absent: false,
+    rating: {
+      rate: 3.9,
+      count: 20
+    }
+  },
+]
